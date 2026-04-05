769671e433192805f67883938f9c67dc515d9db5
Vanessa Pains Fonseca <35502727+VanessaPains@users.noreply.github.com>
2026-04-05 14:42:34 -0300

    Adicionando projeto junit testes automatizados.

---
+++ junit_vanessa_pains/src/main/java/br/edu/iftm/ta/junit_vanessa_pains/JunitVanessaPainsApplication.java
@@ -0,0 +1,13 @@
+package br.edu.iftm.ta.junit_vanessa_pains;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JunitVanessaPainsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JunitVanessaPainsApplication.class, args);
+	}
+
+}
---
+++ junit_vanessa_pains/src/test/java/br/edu/iftm/ta/junit_vanessa_pains/JunitVanessaPainsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.iftm.ta.junit_vanessa_pains;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JunitVanessaPainsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
