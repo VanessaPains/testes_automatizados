cc26fe591d0eeda58c05061376a9bb4bc99e0552
Vanessa Pains Fonseca <35502727+VanessaPains@users.noreply.github.com>
2026-05-24 22:47:59 -0300

    Correção : ID

--- projeto_veterinario_erp/gerenciador-Veterinarios/src/main/java/org/iftm/gerenciadorveterinarios/entities/Animal.java
+++ projeto_veterinario_erp/gerenciador-Veterinarios/src/main/java/org/iftm/gerenciadorveterinarios/entities/Animal.java
@@ -1,7 +1,17 @@
 package org.iftm.gerenciadorveterinarios.entities;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_animal")
 public class Animal {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String nome;
     private String especie;
--- projeto_veterinario_erp/gerenciador-Veterinarios/src/main/java/org/iftm/gerenciadorveterinarios/entities/Funcionario.java
+++ projeto_veterinario_erp/gerenciador-Veterinarios/src/main/java/org/iftm/gerenciadorveterinarios/entities/Funcionario.java
@@ -2,8 +2,18 @@ package org.iftm.gerenciadorveterinarios.entities;
 
 import java.math.BigDecimal;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_funcionario")
 public class Funcionario {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String nome;
     private String cargo;
